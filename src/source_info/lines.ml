52f751f08a6d547212e55cbeeae454aeeb9ddcc0
Paul-Elliot <peada@free.fr>
2022-12-07 11:42:08 +0100

    Ignore line directives for line number
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/source_info/lines.ml
+++ src/source_info/lines.ml
@@ -1,14 +1,21 @@
 let lines_locs src =
   let lines = String.split_on_char '\n' src in
+  let is_line_directive line =
+    if String.length line > 0 then String.get line 0 = '#' else false
+  in
   let _, poses, _ =
     List.fold_left
       (fun (i, poses, count) line ->
         let l = String.length line in
-        let poses = (i, (count, count)) :: poses in
-        (i + 1, poses, count + l + 1))
+        let new_i, new_pos =
+          (* Ignore line directives *)
+          if is_line_directive line then (i, poses)
+          else (i + 1, [(Types.Line i, (count, count))])
+        in
+        (new_i, new_pos @ poses, count + l + 1))
       (1, [], 0) lines
   in
   poses
 
-let split src = lines_locs src |> List.rev_map (fun (x, y) -> (Types.Line x, y))
+let split src = lines_locs src
 (* The order won't matter *)
