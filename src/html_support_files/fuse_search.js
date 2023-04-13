6981244eb8124f3d11638181ed79ab687ea223d7
Emile Trotignon <emile@tarides.com>
2023-04-13 14:58:52 +0200

    seaarch bar style update

--- src/html_support_files/fuse_search.js
+++ src/html_support_files/fuse_search.js
@@ -1,24 +1,31 @@
 document.querySelector(".search-bar").addEventListener("input", (event) => {
-  let results = idx_fuse.search(event.target.value);
-  let search_result = document.querySelector(".search-result");
-  search_result.innerHTML = "";
-  let f = (entry) => {
-    let container = document.createElement("a");
-    container.href = base_url + entry.item.url;
-    container.classList.add("search-entry", entry.item.kind);
+    let results = idx_fuse.search(event.target.value);
+    let search_result = document.querySelector(".search-result");
+    search_result.innerHTML = "";
+    let f = (entry) => {
+        let container = document.createElement("a");
+        container.href = base_url + entry.item.url;
+        container.classList.add("search-entry", entry.item.kind);
+        let title = document.createElement("code");
+        title.classList.add("entry-title");
+        let kind = document.createElement("span");
+        kind.innerText = entry.item.kind;
+        kind.classList.add("entry-kind");
+        let name = document.createElement("span");
+        name.classList.add("entry-name");
+        name.innerText = entry.item.name;
 
-    let name = document.createElement("div");
-    name.classList.add("entry-name");
-    name.innerText = entry.item.name;
+        title.appendChild(kind);
+        title.appendChild(name);
 
-    let comment = document.createElement("div");
-    comment.innerText = entry.item.comment;
-    comment.classList.add("entry-comment");
+        let comment = document.createElement("div");
+        comment.innerText = entry.item.comment;
+        comment.classList.add("entry-comment");
 
-    container.appendChild(name);
-    container.appendChild(comment);
+        container.appendChild(title);
+        container.appendChild(comment);
 
-    search_result.appendChild(container);
-  };
-  results.map(f);
+        search_result.appendChild(container);
+    };
+    results.map(f);
 });
