bbf9b7f262bb662eb7bdcb555f6715c0f8e8da1d
Paul-Elliot <peada@free.fr>
2024-08-22 15:54:08 +0200

    Separate html-generate and html-generate-asset

--- src/odoc/rendering.ml
+++ src/odoc/rendering.ml
@@ -2,66 +2,53 @@ open Odoc_document
 open Or_error
 open Odoc_model
 
-let check_empty_asset_path asset_path filename =
-  if asset_path <> None then
-    Error.raise_warning
-    @@ Error.filename_only
-         "--asset-path only has an effect when generating from an asset"
-         filename
-
-let documents_of_unit ~warnings_options ~syntax ~renderer ~extra ~asset_path
-    ~filename unit =
+let documents_of_unit ~warnings_options ~syntax ~renderer ~extra unit =
   Error.catch_warnings (fun () ->
-      check_empty_asset_path asset_path filename;
       renderer.Renderer.extra_documents extra (CU unit))
   |> Error.handle_warnings ~warnings_options
   >>= fun extra_docs ->
   Ok (Renderer.document_of_compilation_unit ~syntax unit :: extra_docs)
 
-let documents_of_asset ~warnings_options:_ ~asset_path unit =
-  (match asset_path with None -> failwith "TODO" | Some a -> a)
-  |> fun asset_path -> Ok [ Renderer.document_of_asset asset_path unit ]
-
-let documents_of_page ~warnings_options ~syntax ~renderer ~extra ~asset_path
-    ~filename page =
+let documents_of_page ~warnings_options ~syntax ~renderer ~extra page =
   Error.catch_warnings (fun () ->
-      check_empty_asset_path asset_path filename;
       renderer.Renderer.extra_documents extra (Page page))
   |> Error.handle_warnings ~warnings_options
   >>= fun extra_docs -> Ok (Renderer.document_of_page ~syntax page :: extra_docs)
 
-let documents_of_odocl ~warnings_options ~renderer ~extra ~syntax ~asset_path
-    input =
-  let filename = Fpath.filename input in
+let documents_of_odocl ~warnings_options ~renderer ~extra ~syntax input =
   Odoc_file.load input >>= fun unit ->
   match unit.content with
   | Odoc_file.Page_content odoctree ->
-      documents_of_page ~warnings_options ~syntax ~renderer ~extra ~asset_path
-        ~filename odoctree
+      documents_of_page ~warnings_options ~syntax ~renderer ~extra odoctree
+  | Unit_content odoctree ->
+      documents_of_unit ~warnings_options ~syntax ~renderer ~extra odoctree
   | Impl_content _ ->
       Error
         (`Msg
           "Wrong kind of unit: Expected a page or module unit, got an \
            implementation. Use the dedicated command for implementation.")
-  | Unit_content odoctree ->
-      documents_of_unit ~warnings_options ~syntax ~renderer ~extra ~asset_path
-        ~filename odoctree
-  | Asset_content a -> documents_of_asset ~warnings_options ~asset_path a
+  | Asset_content _ ->
+      Error
+        (`Msg
+          "Wrong kind of unit: Expected a page or module unit, got an asset \
+           unit. Use the dedicated command for assets.")
 
 let documents_of_input ~renderer ~extra ~resolver ~warnings_options ~syntax
-    ~asset_path input =
+    input =
   let output = Fs.File.(set_ext ".odocl" input) in
   Odoc_link.from_odoc ~resolver ~warnings_options input output >>= function
   | `Page page -> Ok [ Renderer.document_of_page ~syntax page ]
+  | `Module m -> documents_of_unit ~warnings_options ~syntax ~renderer ~extra m
   | `Impl _ ->
       Error
         (`Msg
           "Wrong kind of unit: Expected a page or module unit, got an \
            implementation. Use the dedicated command for implementation.")
-  | `Module m ->
-      documents_of_unit ~warnings_options ~filename:"" ~syntax ~asset_path
-        ~renderer ~extra m
-  | `Asset a -> documents_of_asset ~warnings_options ~asset_path a
+  | `Asset _ ->
+      Error
+        (`Msg
+          "Wrong kind of unit: Expected a page or module unit, got an asset \
+           unit. Use the dedicated command for assets.")
 
 let render_document renderer ~sidebar ~output:root_dir ~extra_suffix ~extra doc
     =
@@ -95,7 +82,6 @@ let render_odoc ~resolver ~warnings_options ~syntax ~renderer ~output extra file
     =
   let extra_suffix = None in
   documents_of_input ~renderer ~extra ~resolver ~warnings_options ~syntax file
-    ~asset_path:None
   >>= fun docs ->
   List.iter
     (render_document renderer ~sidebar:None ~output ~extra_suffix ~extra)
@@ -103,14 +89,14 @@ let render_odoc ~resolver ~warnings_options ~syntax ~renderer ~output extra file
   Ok ()
 
 let generate_odoc ~syntax ~warnings_options ~renderer ~output ~extra_suffix
-    ~sidebar ~asset_path extra file =
+    ~sidebar extra file =
   (match sidebar with
   | None -> Ok None
   | Some x ->
       Odoc_file.load_index x >>= fun (sidebar, _) ->
       Ok (Some (Odoc_document.Sidebar.of_lang sidebar)))
   >>= fun sidebar ->
-  documents_of_odocl ~warnings_options ~renderer ~extra ~syntax ~asset_path file
+  documents_of_odocl ~warnings_options ~renderer ~extra ~syntax file
   >>= fun docs ->
   List.iter
     (render_document renderer ~output ~sidebar ~extra_suffix ~extra)
@@ -149,15 +135,24 @@ let generate_source_odoc ~syntax ~warnings_options ~renderer ~output
   | Page_content _ | Unit_content _ | Asset_content _ ->
       Error (`Msg "Expected an implementation unit")
 
+let generate_asset_odoc ~warnings_options:_ ~renderer ~output ~asset_file
+    ~extra_suffix extra file =
+  Odoc_file.load file >>= fun unit ->
+  match unit.content with
+  | Odoc_file.Asset_content unit ->
+      let doc = Renderer.document_of_asset asset_file unit in
+      render_document renderer ~output ~sidebar:None ~extra_suffix ~extra doc;
+      Ok ()
+  | Page_content _ | Unit_content _ | Impl_content _ ->
+      Error (`Msg "Expected an asset unit")
+
 let targets_odoc ~resolver ~warnings_options ~syntax ~renderer ~output:root_dir
     ~extra odoctree =
   let docs =
     if Fpath.get_ext odoctree = ".odoc" then
       documents_of_input ~renderer ~extra ~resolver ~warnings_options ~syntax
-        ~asset_path:None odoctree
-    else
-      documents_of_odocl ~warnings_options ~renderer ~extra ~syntax
-        ~asset_path:None odoctree
+        odoctree
+    else documents_of_odocl ~warnings_options ~renderer ~extra ~syntax odoctree
   in
   docs >>= fun docs ->
   List.iter
@@ -188,18 +183,3 @@ let targets_source_odoc ~syntax ~warnings_options ~renderer ~output:root_dir
       Ok ()
   | Page_content _ | Unit_content _ | Asset_content _ ->
       Error (`Msg "Expected an implementation unit")
-(* let docs = *)
-(*   if Fpath.get_ext odoctree = ".odoc" then *)
-(*     documents_of_input ~renderer ~extra ~resolver ~warnings_options ~syntax *)
-(*       odoctree *)
-(*   else documents_of_odocl ~warnings_options ~renderer ~extra ~syntax odoctree *)
-(* in *)
-(* docs >>= fun docs -> *)
-(* List.iter *)
-(*   (fun doc -> *)
-(*     let pages = renderer.Renderer.render extra None doc in *)
-(*     Renderer.traverse pages ~f:(fun filename _content -> *)
-(*         let filename = Fpath.normalize @@ Fs.File.append root_dir filename in *)
-(*         Format.printf "%a\n" Fpath.pp filename)) *)
-(*   docs; *)
-(* Ok () *)
