214bdfe970ca23a0cae3948c02c80943635befae
Paul-Elliot <peada@free.fr>
2024-08-29 16:05:36 +0200

    Clean up commented out code

--- src/document/sidebar.ml
+++ src/document/sidebar.ml
@@ -5,25 +5,22 @@ let sidebar_toc_entry id content =
   let target = Target.Internal (Resolved href) in
   inline @@ Inline.Link { target; content; tooltip = None }
 
-module Hierarchy
-(*  : sig *)
-(*   type 'a dir *)
-(*   (\** Directory in a filesystem-like abstraction, where files have a ['a] *)
-(*       payload and directory can also have a ['a] payload. *\) *)
+module Hierarchy : sig
+  type 'a dir
+  (** Directory in a filesystem-like abstraction, where files have a ['a]
+      payload and directory can also have a ['a] payload. *)
 
-(*   val make : ('a * string list) list -> 'a dir *)
-(*   (\** Create a directory from a list of payload and file path (given as a *)
-(*       string list). Files named ["index"] give their payload to their *)
-(*       containing directory. *\) *)
+  val of_lang :
+    Odoc_index.Index.index_payload Odoc_index.Index.PageForest.t ->
+    (Url.Path.t * Inline.one) option dir
 
-(*   val remove_common_root : 'a dir -> 'a dir *)
-(*   (\** Returns the deepest subdir containing all files. *\) *)
+  val remove_common_root : 'a dir -> 'a dir
+  (** Returns the deepest subdir containing all files. *)
 
-(*   val to_sidebar : ?fallback:string -> ('a -> Block.one) -> 'a dir -> Block.t *)
-(* end *) =
-struct
-  type 'a dir = Directory of 'a (* option *) * (* string *  *) 'a dir list
-  (* and 'a t = Leaf of 'a | Dir of 'a dir *)
+  val to_sidebar :
+    ?fallback:string -> ('a -> Block.one) -> 'a option dir -> Block.t
+end = struct
+  type 'a dir = Directory of 'a * 'a dir list
 
   open Odoc_index.Index
 
@@ -97,28 +94,6 @@ struct
 
     of_lang ~parent_id:None dir
 
-  (* let rec add_entry_to_dir (dir : 'a dir) payload path = *)
-  (*   match (path, dir) with *)
-  (*   | [], _ -> assert false *)
-  (*   | [ "index" ], (None, l) -> (Some payload, l) *)
-  (*   | [ name ], (p, l) -> (p, (name, Leaf payload) :: l) *)
-  (*   | name :: rest, (p, l) -> *)
-  (*       let rec add_to_dir (l : (string * 'a t) list) = *)
-  (*         match l with *)
-  (*         | [] -> [ (name, Dir (add_entry_to_dir (None, []) payload rest)) ] *)
-  (*         | (name2, Dir d) :: q when name = name2 -> *)
-  (*             (name2, Dir (add_entry_to_dir d payload rest)) :: q *)
-  (*         | d :: q -> d :: add_to_dir q *)
-  (*       in *)
-  (*       (p, add_to_dir l) *)
-
-  (* let make l = *)
-  (*   let empty = (None, []) in *)
-  (*   let add_entry_to_dir acc (path, payload) = *)
-  (*     add_entry_to_dir acc path payload *)
-  (*   in *)
-  (*   List.fold_left add_entry_to_dir empty l *)
-
   let rec remove_common_root = function
     | Directory (_, [ d ]) -> remove_common_root d
     | x -> x
@@ -137,22 +112,6 @@ struct
           [ block (Block.List (Block.Unordered, content)) ]
     in
     name :: content
-
-  (* let rec to_sidebar ?(fallback = "root") convert (name, content) = *)
-  (*   let name = *)
-  (*     match name with *)
-  (*     | Some v -> convert v *)
-  (*     | None -> block (Block.Inline [ inline (Text fallback) ]) *)
-  (*   in *)
-  (*   let content = *)
-  (*     let content = List.map (t_to_sidebar convert) content in *)
-  (*     block (Block.List (Block.Unordered, content)) *)
-  (*   in *)
-  (*   [ name; content ] *)
-
-  (* and t_to_sidebar convert = function *)
-  (*   | _, Leaf payload -> [ convert payload ] *)
-  (*   | fallback, Dir d -> to_sidebar ~fallback convert d *)
 end
 type pages = {
   name : string;
@@ -165,19 +124,7 @@ type t = { pages : pages list; libraries : library list }
 let of_lang (v : Odoc_index.Index.Sidebar.t) =
   let pages =
     let page_hierarchy { Odoc_index.Index.Sidebar.ph_name; pages } =
-      (* if Odoc_index.Index.H.length pages = 0 then None *)
-      (* else *)
-      (* let prepare_for_hierarchy { Odoc_model.Lang.Sidebar.title; id } = *)
-      (*   let path = Url.Path.from_identifier id in *)
-      (*   let payload = *)
-      (*     let content = Comment.link_content title in *)
-      (*     (path, sidebar_toc_entry id content) *)
-      (*   in *)
-      (*   (payload, path |> Url.Path.to_list |> List.map snd) *)
-      (* in *)
-      (* let pages = List.map prepare_for_hierarchy pages in *)
       let hierarchy = Hierarchy.of_lang pages |> Hierarchy.remove_common_root in
-      (* let hierarchy = Hierarchy.make pages |> Hierarchy.remove_common_root in *)
       Some { name = ph_name; pages = hierarchy }
     in
     Odoc_utils.List.filter_map page_hierarchy v.pages
